4d282566caef7d31c79fb97127cc4c5e98da2115
IvanUribe <ivan.uribe@pragma.com.co>
2023-03-23 10:14:25 -0500

    Hola Mundo

---
+++ src/main/java/com/pragma/market/PragmaMarketApplication.java
@@ -0,0 +1,12 @@
+package com.pragma.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragmaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragmaMarketApplication.class, args);
+	}
+}
